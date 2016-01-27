2b70baed00ba3fe8ce5b303df39a68c93179ee00
marvinnn <marvin12@mhs.if.its.ac.id>
2016-01-26 22:05:16 -0800

    Create recyclerView prototype in home fragment

--- app/src/main/java/com/example/revmedia/tugasakhir/HomeFragment.java
+++ app/src/main/java/com/example/revmedia/tugasakhir/HomeFragment.java
@@ -2,9 +2,14 @@ package com.example.revmedia.tugasakhir;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Marvin Zeson on 1/25/2016.
@@ -21,8 +26,32 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        RecyclerView recList = (RecyclerView) rootView.findViewById(R.id.homeCardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        ContactAdapter adapter = new ContactAdapter(createList(30));
+        recList.setAdapter(adapter);
+        return rootView;
+        //return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    private List<ContactInfo> createList(int size) {
 
+        List<ContactInfo> result = new ArrayList<ContactInfo>();
+        for (int i=1; i <= size; i++) {
+            ContactInfo ci = new ContactInfo();
+            ci.name = ContactInfo.NAME_PREFIX + i;
+            ci.surname = ContactInfo.SURNAME_PREFIX + i;
+            ci.email = ContactInfo.EMAIL_PREFIX + i + "@test.com";
+
+            result.add(ci);
+
+        }
+
+        return result;
+    }
 }
--- app/src/main/java/com/example/revmedia/tugasakhir/MainActivity.java
+++ app/src/main/java/com/example/revmedia/tugasakhir/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = MainActivity.class.getSimpleName();
