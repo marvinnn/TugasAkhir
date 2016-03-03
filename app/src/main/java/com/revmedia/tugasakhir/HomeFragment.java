1842758a003e71f1d1a3a9b8eb724550f521f50b
marvinnn <marvin12@mhs.if.its.ac.id>
2016-03-03 09:04:19 +0700

    Connect with Firebase

--- app/src/main/java/com/revmedia/tugasakhir/HomeFragment.java
+++ app/src/main/java/com/revmedia/tugasakhir/HomeFragment.java
@@ -2,13 +2,16 @@ package com.revmedia.tugasakhir;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.revmedia.tugasakhir.R;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,14 +32,34 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        RecyclerView recList = (RecyclerView) rootView.findViewById(R.id.homeCardList);
-        recList.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
+        ListView recList = (ListView) rootView.findViewById(R.id.homeCardList);
+        final ArrayAdapter adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1);
 
-        ContactAdapter adapter = new ContactAdapter(createList(30));
+        //ContactAdapter adapter = new ContactAdapter(createList(30));
         recList.setAdapter(adapter);
+
+        Firebase.setAndroidContext(getActivity());
+
+        new Firebase("https://tugasakhir.firebaseio.com/Artikel")
+                .addChildEventListener(new ChildEventListener() {
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        adapter.add((String) dataSnapshot.child("Judul").getValue());
+                    }
+
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        adapter.remove((String) dataSnapshot.child("Judul").getValue());
+                    }
+
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onCancelled(FirebaseError firebaseError) {
+                    }
+                });
         return rootView;
         //return inflater.inflate(R.layout.fragment_home, container, false);
     }
